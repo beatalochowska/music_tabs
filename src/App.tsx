82d51d4dacadb42598534c1c8457d50e460c09b5
Beata ochowska <blochowska@polcam.com>
2020-08-26 13:11:15 +0200

    adding react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { MainPage } from "pages";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
-  return <MainPage />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/music_tabs" component={MainPage} />
+        <Route path="*" component={MainPage} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
