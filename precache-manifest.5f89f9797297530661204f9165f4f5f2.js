d09cd2b844fb1e05935e2b53070cbd8c2cc794f1
Beata ochowska <blochowska@polcam.com>
2020-08-26 11:34:07 +0200

    Updates

---
+++ precache-manifest.5f89f9797297530661204f9165f4f5f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a39820b2eadea4078d719ec80d9a3f4a",
+    "url": "/music_tabs/index.html"
+  },
+  {
+    "revision": "93428048723708e521fa",
+    "url": "/music_tabs/static/css/main.68c28fc5.chunk.css"
+  },
+  {
+    "revision": "61812f58c9aa91f6ad94",
+    "url": "/music_tabs/static/js/2.b091555e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/music_tabs/static/js/2.b091555e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93428048723708e521fa",
+    "url": "/music_tabs/static/js/main.52529bcd.chunk.js"
+  },
+  {
+    "revision": "0d9342e2a5b3ed77b4a9",
+    "url": "/music_tabs/static/js/runtime-main.b975cb01.js"
+  }
+]);
